1803a7b87100332a5647aa33f3714404fed3255d
imshashwatsingh <102756237+imshashwatsingh@users.noreply.github.com>
2023-01-31 12:13:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+// jshint esversion:6
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const request = require("request");
+
+const https = require("https");
+const { dirname } = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us9.api.mailchimp.com/3.0/lists/f90b5ad078";
+
+  const options = {
+    method: "POST",
+    auth: "shashwat007:2b47139351a44f06309a4b8a8b24b2e8-us9",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      // res.send("Successfully Subscribed !");
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      // res.send("There was an error in signing up, please try again later !");
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("server rumming on port 3000");
+});
+
+// api key
+// 2b47139351a44f06309a4b8a8b24b2e8-us9
+
+// audience id
+//  f90b5ad078
